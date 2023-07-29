4e02babbf6b52c42029a76b0eaee28a06f9abef6
Luca Bernacchia <lucabernacchia98@gmail.com>
2023-07-29 14:41:52 -0300

    ajax delete

--- crud-app/src/main/resources/static/js/main.js
+++ crud-app/src/main/resources/static/js/main.js
@@ -1,16 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
   const showModal = (employeeName, employeeLastName, url) => {
     const deleteModal = document.getElementById('deleteModal');
-    deleteModal.style.display = 'block';
-    document.getElementById('employeeName').textContent = employeeName;
-    document.getElementById('employeeLastName').textContent = employeeLastName;
-
     const acceptButton = deleteModal.querySelector('#acceptButton');
     const cancelButton = deleteModal.querySelector('#cancelButton');
     const xButton = deleteModal.querySelector('#xButton');
 
+    document.getElementById('employeeName').textContent = employeeName;
+    document.getElementById('employeeLastName').textContent = employeeLastName;
+
     acceptButton.addEventListener('click', () => {
-      window.location.href = url;
+      fetch(url, {
+        method: 'GET'
+      })
+      .then(response => {
+        if (response.ok) {
+          closeModal();
+          showDeleteAlert();
+        } else {
+          console.error('Error al eliminar empleado');
+        }
+      })
+      .catch(error => {
+        console.error('Error en la petición de eliminación');
+      });
     });
 
     const closeModal = () => {
@@ -19,6 +31,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     cancelButton.addEventListener('click', closeModal);
     xButton.addEventListener('click', closeModal);
+
+    deleteModal.style.display = 'block';
+  };
+
+  const showDeleteAlert = () => {
+    const deleteAlert = document.getElementById('deleteAlert');
+    deleteAlert.style.display = 'block';
+
+    setTimeout(() => {
+      deleteAlert.style.display = 'none';
+    }, 3000);
   };
 
   const deleteButtons = document.querySelectorAll('a[name="deleteButton"]');
