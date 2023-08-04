b064fbf063eeaeff95b14b3c971c394b01a328f9
Luca Bernacchia <lucabernacchia98@gmail.com>
2023-08-03 22:07:46 -0300

    fix modal

--- crud-app/src/main/resources/static/js/ajaxSearch.js
+++ crud-app/src/main/resources/static/js/ajaxSearch.js
@@ -5,7 +5,6 @@ function performAjaxRequest(formData) {
     data: formData,
     success: function (response) {
       $("#listaDeEmpleadosContainer").html(response);
-      attachDeleteButtonListeners();
     },
     error: function (xhr, status, error) {
       console.error(error);
@@ -13,7 +12,20 @@ function performAjaxRequest(formData) {
   });
 }
 
+function attachDeleteButtonListeners() {
+  // Delegación de eventos para vincular eventos de botones de eliminación
+  $("#listaDeEmpleadosContainer").on("click", 'a[name="deleteButton"]', function (event) {
+    event.preventDefault();
+    const url = $(this).attr('href');
+    const employeeName = $(this).closest('tr').find('.employee-name').eq(0).text();
+    const employeeLastName = $(this).closest('tr').find('.employee-name').eq(1).text();
+    showModal(employeeName, employeeLastName, url);
+  });
+}
+
 $(document).ready(function () {
+  attachDeleteButtonListeners();
+
   $("#filtroSelect").on("change", function () {
     const formData = $("#filtroEmpleadosForm").serialize();
     performAjaxRequest(formData);
